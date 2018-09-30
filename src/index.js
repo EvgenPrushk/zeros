ffd3847d745f323cb2c164ff1037cdfd9818698e
EvgenPrushk <ogmprushak@mail.ru>
2018-09-30 22:27:25 +0300

    Task 7

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  /*реализован через сортировки сосисок и булочек информация https://site.ada.edu.az/~medv/acm/Docs%20e-olimp/Volume%202/123.html
+  https://tproger.ru/problems/how-many-zeros-at-the-end-of-the-factorial-of-100/ */
+  zero = 0
+  for (var i = 1; i <= number; i++) {
+    counter = i;
+    while (counter%5 == 0) {
+      counter /= 5;
+      zero ++
+
+    }
+  }
+  return zero
 }
+
+
